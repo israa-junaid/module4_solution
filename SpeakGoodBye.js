8c8e632154505959459626316c980c6e5dad4ee9
israa-junaid <63905901+israa-junaid@users.noreply.github.com>
2020-05-18 16:33:06 +0500

    Add files via upload

---
+++ SpeakGoodBye.js
@@ -0,0 +1,13 @@
+(function(window){
+  var speakWord = "Good Bye";
+  var byeSpeaker =new Object();
+  byeSpeaker.speak=function speak(name){
+    console.log(speakWord + " " +name);
+  };
+  window.byeSpeaker=byeSpeaker;
+ 
+})(window);
+
+
+
+
